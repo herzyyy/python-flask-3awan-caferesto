7608de72d4e23c2de2d37e2a149f000c2b77873a
Helmi Rizki <helmirizkipauzi@gmail.com>
2025-11-07 17:38:53 +0700

    menambah cors

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from config.database import Base, engine
 from routes.route import menu_bp
 from routes.route import transaction_bp
 
 app = Flask(__name__)
+
+# ✅ Aktifkan CORS agar Flutter bisa akses API
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Daftarkan semua blueprint
 app.register_blueprint(menu_bp)
@@ -14,6 +18,5 @@ Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     import os
-
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
