eebbfbf0dec849194d07d20bddcb1e0c828ad419
Helmi Rizki <helmirizkipauzi@gmail.com>
2025-11-08 01:27:15 +0700

    kembali ke sebelumnya

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 from flask_cors import CORS
 from config.database import Base, engine
-from routes.route import menu_bp, transaction_bp
-import os
+from routes.route import menu_bp
+from routes.route import transaction_bp
 
 app = Flask(__name__)
 
-# ✅ Aktifkan CORS agar Flutter bisa akses API dan file gambar
+# ✅ Aktifkan CORS agar Flutter bisa akses API
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# ✅ Daftarkan semua blueprint
+# Daftarkan semua blueprint
 app.register_blueprint(menu_bp)
 app.register_blueprint(transaction_bp)
 
-# ✅ Endpoint tambahan untuk serve gambar dari folder static/uploads
-@app.route('/static/uploads/<path:filename>')
-def uploaded_file(filename):
-    upload_folder = os.path.join(app.root_path, 'static', 'uploads')
-    return send_from_directory(upload_folder, filename)
-
-# ✅ Buat tabel otomatis kalau belum ada
+# Buat tabel otomatis kalau belum ada
 Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
